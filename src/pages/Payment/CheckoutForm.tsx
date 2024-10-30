0fbb3f54bbd31620bb8038c877e00a684d014413
nishulabiba <nishlabiba@gmail.com>
2024-10-30 23:34:59 +0600

    payment gateway updated

--- src/pages/Payment/CheckoutForm.tsx
+++ src/pages/Payment/CheckoutForm.tsx
@@ -1,9 +1,13 @@
 import { CardElement, useStripe } from "@stripe/react-stripe-js";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { useCart } from "../../hooks/useCart";
 
 
 const CheckoutForm = ({ price, prod }: { price: number; prod: unknown[] } ) => {
     const stripe = useStripe();
-    
+    const navigate = useNavigate();
+    const {setCartItems}= useCart()
 
     const handleSubmit = (event: any) => {
         event.preventDefault();
@@ -11,6 +15,14 @@ const CheckoutForm = ({ price, prod }: { price: number; prod: unknown[] } ) => {
 
         console.log(event.target);
     };
+    const handleClick = ()=>{
+        Swal.fire({
+            text: "Payment successful! ",
+            color: "green"
+        })
+        navigate("/");
+        setCartItems([]);
+    }
 
     return (
         <div>
@@ -34,8 +46,8 @@ const CheckoutForm = ({ price, prod }: { price: number; prod: unknown[] } ) => {
                         },
                     }}
                 />
-                <button className="btn btn-primary btn-outline mt-4" type="submit" disabled={!stripe}>
-                    Pay
+                <button onClick={handleClick} className="btn btn-primary btn-outline mt-4" type="submit" disabled={!stripe} >
+                    <Link to="/">Pay</Link>
                 </button>
             </form>
         </div>
